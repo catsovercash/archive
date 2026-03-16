6f9fa7b3cd05e191e1950e80fbb9cb456ca8fec7
jxhn <50688138+catsovercash@users.noreply.github.com>
2026-03-16 21:47:54 +0800

    Create main.cpp

---
+++ C++/bubble-sort/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void sortArr(int arr[], int size)
+{
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+
+		}
+	}
+
+}
+
+void printArr(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int main()
+{
+
+	int size;
+
+	cin >> size;
+
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sortArr(arr, size);
+	printArr(arr, size);
+
+}
